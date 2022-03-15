b49984c60012c5d947f006572bd77d543068d0ff
rasmu31 <101635391+rasmu31@users.noreply.github.com>
2022-03-15 19:04:45 +0100

    Update add-wan-route-safe.ps1

--- add-wan-route-safe.ps1
+++ add-wan-route-safe.ps1
@@ -7,7 +7,7 @@ $record=""
 
 $record_result = invoke-restmethod -method get -uri "https://api.cloudflare.com/client/v4/zones/$zone/dns_records/$record" -Headers $Headers
 $ip = $record_result.result.content
-$interface = "192.168.1.1"
+$gateway = "192.168.1.1"
 
 try {
 	Get-NetRoute -DestinationPrefix "$ip/32" -ErrorAction Stop
@@ -19,4 +19,4 @@ catch {
 	
 }
 
-route -p add $ip mask 255.255.255.255 $interface
+route -p add $ip mask 255.255.255.255 $gateway
